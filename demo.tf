32252e050a61f36f1ccbadc1b95d2f5bbd6e4323
Adam Björnberg <adam.bjornberg@outlook.com>
2020-05-10 21:30:17 +0200

    Add demo files

--- demo.tf
+++ demo.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-  version = "=2.8.0"
+  version         = "=2.8.0"
   features {}
   subscription_id = var.azure-subscription-id
   client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret 
+  client_secret   = var.azure-client-secret
   tenant_id       = var.azure-tenant-id
 }
 
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "example-2" {
   location            = "East US"
 
   subnet {
-    name           = "AdamSubnet1"
+    name           = "Adamsubnet1"
     address_prefix = "10.1.0.0/24"
   }
 }
@@ -50,20 +50,36 @@ resource "azurerm_virtual_network_peering" "example-2" {
   remote_virtual_network_id = azurerm_virtual_network.example-1.id
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "VM1"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  vm_size               = "Standard_D2s_v3"
+resource "azurerm_network_interface" "example" {
+  name                = "vm1251"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-  storage_image_reference {
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = tolist(azurerm_virtual_network.example-2.subnet)[0].id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                  = "VM1"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  size                  = "Standard_D2s_v3"
+  admin_username        = "Helal"
+  admin_password        = "Ab1234567890"
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2016-Datacenter"
     version   = "latest"
-  }
-  os_profile {
-    admin_username = "Helal"
-    admin_password = "Ab1234567890"
   }
 }
